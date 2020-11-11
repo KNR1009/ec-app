f5a0e9e0ed3ab3af88366a069e19ddfd8a2f0ef0
aponasi178cm <itokazunari5375@icloud.com>
2020-11-11 23:00:05 +0900

    画像アップロードをfirebaseへ保存

--- src/componets/Producuts/ImageArea.jsx
+++ src/componets/Producuts/ImageArea.jsx
@@ -1,53 +1,52 @@
-import React, {useCallback} from 'react'
+import React, { useCallback } from "react";
 import IconButton from "@material-ui/core/IconButton";
 import AddPhotoAlternateIcon from "@material-ui/icons/AddPhotoAlternate";
 import {storage} from '../../firebase/index'
 
+const ImageArea = (props) => {
 
-const uploadImage = useCallback(
-  (event) => {
-    dispatch(showLoadingAction("uploading..."));
-    const file = event.target.files;
-    let blob = new Blob(file, { type: "image/jpeg" });
+  const uploadImage = useCallback(
+    (event) => {
+      const file = event.target.files;
+      let blob = new Blob(file, { type: "image/jpeg" });
 
-    // Generate random 16 digits strings
-    const S = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    const N = 16;
-    const fileName = Array.from(crypto.getRandomValues(new Uint32Array(N)))
-      .map((n) => S[n % S.length])
-      .join("");
+      // Generate random 16 digits strings
+      const S =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+      const N = 16;
+      const fileName = Array.from(crypto.getRandomValues(new Uint32Array(N)))
+        .map((n) => S[n % S.length])
+        .join("");
 
-    const uploadRef = storage.ref("images").child(fileName);
-    const uploadTask = uploadRef.put(blob);
+      const uploadRef = storage.ref("images").child(fileName);
+      const uploadTask = uploadRef.put(blob);
 
-    uploadTask
-      .then(() => {
+      uploadTask.then(() => {
         // Handle successful uploads on complete
         uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
           const newImage = { id: fileName, path: downloadURL };
           props.setImages((prevState) => [...prevState, newImage]);
-          dispatch(hideLoadingAction());
         });
-      })
-      .catch(() => {
-        dispatch(hideLoadingAction());
       });
-  },
-  [props.setImages]
-);
+    },
+    [props.setImages]
+  );
 
-
-
-const ImageArea = (props) => {
-  return(
+  return (
     <div className="u-text-right">
       <span>商品画像を登録する</span>
-        <IconButton>
+      <IconButton>
+        <label>
           <AddPhotoAlternateIcon />
-          <input type="file" className="u-display-none" id="image" />
-        </IconButton>
+          <input
+            className="u-display-none"
+            type="file"
+            id="image"
+            onChange={(event) => uploadImage(event)}
+          />
+        </label>
+      </IconButton>
     </div>
-  )
+  );
 }
-
 export default ImageArea
