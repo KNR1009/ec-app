3c68cf26a1013de5196ceb06507af16393af8099
aponasi178cm <itokazunari5375@icloud.com>
2020-12-04 21:57:15 +0900

    注文履歴の日付を取得するメソットの修正

--- src/componets/Producuts/OrderHistoryItem.jsx
+++ src/componets/Producuts/OrderHistoryItem.jsx
@@ -3,21 +3,25 @@ import Divider from "@material-ui/core/Divider";
 // import {OrderedProducts} from "./index";
 // import {datetimeToString, dateToString} from "../../function/common";
 import {TextDetail} from "../UIkit";
+import { OrderProducts } from '../Producuts/index'
 
 const OrderHistoryItem = (props) => {
 
   // 注文した日時を取得して変換する関数
-  const datetimeTostring = (date) => {
-    const year = order.updated_at.toDate().getFullYear()
-    const Month = order.updated_at.toDate().getMonth()+1
-    const day = order.updated_at.toDate().getDate()
-    return year + '年' + Month + '月' + day + '日'
-  }
+const datetimeTostring = (dt) => {
+    return dt.getFullYear() + '年'
+        + ((dt.getMonth()+1)) + '月'
+        + (dt.getDate()) + '日'
+        
+};
   
   const order = props.order;
   const updated = datetimeTostring(order.updated_at.toDate())
   const shipping = datetimeTostring(order.shipping_date.toDate())
   const total = '¥' + order.amount.toLocaleString()
+  const products = order.products
+
+
 
 
   return(
@@ -27,6 +31,9 @@ const OrderHistoryItem = (props) => {
         <TextDetail label={'注文日時'} value={updated}></TextDetail>
         <TextDetail label={'発送日'} value={shipping}></TextDetail>
         <TextDetail label={'注文合計'} value={total}></TextDetail>
+        {order.products.length > 0 && (
+          <OrderProducts products={products}/>
+        )}
         <div className="module-spacer--extra-extra-small" />
         <Divider />
     </div>
---
+++ src/componets/Producuts/OrderProducts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const OrderProducts = (props) => {
+
+  return(
+    <>
+     
+    </>
+  )
+}
+
+export default OrderProducts
