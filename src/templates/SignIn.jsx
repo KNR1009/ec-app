9b6e1dcc98cd195a00aff07aceb722f821cef51b
KNR1009 <itokazunari5375@icloud.com>
2021-01-25 22:42:22 +0900

    ログイン画面のデザイン変更

--- src/templates/SignIn.jsx
+++ src/templates/SignIn.jsx
@@ -1,16 +1,72 @@
 import React, { useState, useCallback } from "react";
-import { TextInput, PrimaryButton } from "../componets/UIkit/index";
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 import { signIn } from "../reducks/users/operations";
 import { useDispatch } from "react-redux";
 import { push } from "connected-react-router";
-import styled from "@emotion/styled";
 
-const SignIn = () => {
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://images.pexels.com/photos/880459/pexels-photo-880459.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignInSide() {
+  const classes = useStyles();
   const dispatch = useDispatch();
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  // 入力値時にstateを変更するメソット
   const inputEmail = useCallback(
     (e) => {
       setEmail(e.target.value);
@@ -26,58 +82,69 @@ const SignIn = () => {
   );
 
   return (
-    <div className="c-section-container">
-      <h2 className="u-text-center u-text__headline">ログイン</h2>
-      <div className="module-spacer--medium"></div>
-
-      <TextInput
-        fullWidth={true}
-        label={"メールアドレス"}
-        multiline={false}
-        required={true}
-        rows={1}
-        value={email}
-        type={"email"}
-        onChange={inputEmail}
-      />
-      <TextInput
-        fullWidth={true}
-        label={"パスワード"}
-        multiline={false}
-        required={true}
-        rows={1}
-        value={password}
-        type={"password"}
-        onChange={inputPassword}
-      />
-
-      <div className="module-spacer--medium"></div>
-      <div className="center">
-        <PrimaryButton
-          label={"ログイン"}
-          onClick={() => dispatch(signIn(email, password))}
-        />
-        <div className="module-spacer--medium"></div>
-        <Option>
-          <p className="noaccount" onClick={() => dispatch(push("/signup"))}>
-            アカウントを持っていない方はこちら
-          </p>
-          <p
-            className="passwordrest"
-            onClick={() => dispatch(push("/signin/reset"))}
-          >
-            パスワードを忘れた方はこちら
-          </p>
-        </Option>
-      </div>
-    </div>
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            ログイン
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="メールアドレス"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={email}
+              onChange={inputEmail}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="パスワード"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={inputPassword}
+            />
+           
+            <Button
+              disabled={! email || password.length < 6}
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={() => dispatch(signIn(email, password))}
+            >
+              ログイン
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2" onClick={() => dispatch(push("/signup"))}>
+                  新規アカウント登録
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2" onClick={() => dispatch(push("/signin/reset"))}>
+                  パスワードを忘れた方はこちら
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
   );
-};
-
-const Option = styled.div`
-  p {
-    cursor: pointer;
-  }
-`;
-
-export default SignIn;
+}
